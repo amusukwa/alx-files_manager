5196ad59e76bb9a04b6c566b838d8e225d7fcd43
amusukwa <amusukwa@gmail.com>
2024-05-10 19:12:20 +0000

    db.js

---
+++ utils/db.js
@@ -0,0 +1,35 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+    constructor() {
+        const {
+            DB_HOST = 'localhost',
+            DB_PORT = 27017,
+            DB_DATABASE = 'files_manager',
+        } = process.env;
+
+        this.host = DB_HOST;
+        this.port = DB_PORT;
+        this.database = DB_DATABASE;
+        this.client = new MongoClient(`mongodb://${this.host}:${this.port}`, { useUnifiedTopology: true });
+        this.client.connect();
+        this.db = this.client.db(this.database);
+    }
+
+    isAlive() {
+        return !!this.client && !!this.client.topology && this.client.topology.isConnected();
+    }
+
+    async nbUsers() {
+        return this.db.collection('users').countDocuments();
+    }
+
+    async nbFiles() {
+        return this.db.collection('files').countDocuments();
+    }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
+
