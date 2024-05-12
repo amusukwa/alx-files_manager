bdef32c8a1080e2fc23acd05abc7a3e9ebcdff61
amusukwa <amusukwa@gmail.com>
2024-05-12 19:34:37 +0000

    index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
@@ -13,6 +14,9 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 
 export default router;
