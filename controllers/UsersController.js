a20c905e648791835bdec676aca6dd85783ebfe4
amusukwa <amusukwa@gmail.com>
2024-05-12 10:29:28 +0000

    task 4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,11 +1,11 @@
+import { v4 as uuidv4 } from 'uuid';
 import sha1 from 'sha1';
+import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class UsersController {
     static async postNew(req, res) {
         const { email, password } = req.body;
-
-        // Check if email and password are provided
         if (!email) {
             return res.status(400).json({ error: 'Missing email' });
         }
@@ -29,18 +29,39 @@ class UsersController {
         };
 
         try {
-            // Insert the new user into the database
-            const result = await dbClient.db.collection('users').insertOne(newUser);
-            const { _id } = result.ops[0]; // Extract the auto-generated id
 
-            // Return the new user with only email and id
+            const result = await dbClient.db.collection('users').insertOne(newUser);
+            const { _id } = result.ops[0]; 
             return res.status(201).json({ id: _id, email });
         } catch (error) {
             console.error('Error creating user:', error);
             return res.status(500).json({ error: 'Internal Server Error' });
         }
     }
+
+    static async getMe(req, res) {
+        const token = req.headers['x-token'];
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const key = `auth_${token}`;
+        const userId = await redisClient.get(key);
+
+        if (!userId) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const user = await dbClient.db.collection('users').findOne({ _id: userId });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        return res.status(200).json({ id: user._id, email: user.email });
+    }
 }
 
 export default UsersController;
+
 
